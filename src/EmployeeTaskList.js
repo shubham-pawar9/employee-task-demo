fdb690dc850b2aa3fe8ee12008600203b70086b7
shubham-pawar9 <developer.shubhampawar@gmail.com>
2023-10-08 21:39:30 +0530

    updated code

---
+++ src/EmployeeTaskList.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+function EmployeeTaskList({ employeeList, onClose }) {
+  const [employeeId, setEmployeeId] = useState();
+  const [selectedEmployeeTask, setSelectedEmployeeTask] = useState("");
+  const handleTaskList = () => {
+    // employeeList &&
+    //   employeeList.map((value) => {
+    //     if (employeeId == value.employeeId) {
+    //       return setSelectedEmployeeTask(value.additionalData);
+    //     } else {
+    //       alert("Id not in Employee List, First Add Employee ID")
+    //     }
+    //   });
+    console.log(employeeList);
+    const trimmedEmployeeId = employeeId.trim();
+    const foundEmployee = employeeList.find(
+      (value) => value.employeeId == trimmedEmployeeId
+    );
+    console.log(employeeList, employeeId);
+    console.log(foundEmployee);
+    if (foundEmployee) {
+      setSelectedEmployeeTask(foundEmployee.additionalData);
+    } else {
+      alert("Id not in Employee List, First Add Employee ID");
+    }
+  };
+  return (
+    <div className="popupTaskList">
+      <input type="number" onChange={(e) => setEmployeeId(e.target.value)} />
+      <button className="taskSubmit" onClick={handleTaskList}>
+        Submit
+      </button>
+      <button className="close" onClick={onClose}>
+        X
+      </button>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Task</th>
+            </tr>
+          </thead>
+          <tbody>
+            {selectedEmployeeTask &&
+              selectedEmployeeTask.map((data) => {
+                return (
+                  <tr key={data.date}>
+                    <td>{data.date}</td>
+                    <td>{data.data}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default EmployeeTaskList;
